656b29283ee57fc260f642e902e1400ee526eef0
Enrico Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-12-05 10:49:35 -0500

    Update Q1Questions.py

--- Q1Questions.py
+++ Q1Questions.py
@@ -11,24 +11,29 @@ while True:
     question = str(q)+". "+user+"\n"
     print('Enter 4 answers, and choose the correct answer (A,B,C or D), or enter "EXIT" to quit and save. (E = Correct Answer Letter)')
     for i in range(5):
-        user = input(chr(65+i)+": ")
+        user = input(str(i+1)+": ")
         if user == "EXIT":
             questionstxt.close()
             exit()
         if i==4:
-            if str.capitalize(user)!="A" and str.capitalize(user)!="B" and str.capitalize(user)!="C" and str.capitalize(user)!="D":
-                while True:
-                    print('Please type A,B,C or D.')
-                    user = input('E: ')
-                    if user == "EXIT":
-                        questionstxt.close()
-                        exit()
-                    print(user)
-                    if str.capitalize(user)=="A" or str.capitalize(user)=="B" or str.capitalize(user)=="C" or str.capitalize(user)=="D":
-                        question = question+chr(65+i)+": "+str.capitalize(user)+"\n"
-                        break
-            else:
-                question = question+chr(65+i)+": "+str.capitalize(user)+"\n"
+            try:
+                if int(user)>0 and int(user)<5:
+                    question = question+chr(65+i)+": "+user+"\n"
+                
+        #if i==4:
+        #    if str.capitalize(user)!="A" and str.capitalize(user)!="B" and str.capitalize(user)!="C" and str.capitalize(user)!="D":
+        #        while True:
+        #            print('Please type A,B,C or D.')
+        #            user = input('E: ')
+        #            if user == "EXIT":
+        #                questionstxt.close()
+        #                exit()
+        #            print(user)
+        #            if str.capitalize(user)=="A" or str.capitalize(user)=="B" or str.capitalize(user)=="C" or str.capitalize(user)=="D":
+        #                question = question+chr(65+i)+": "+str.capitalize(user)+"\n"
+        #                break
+        #    else:
+        #        question = question+chr(65+i)+": "+str.capitalize(user)+"\n"
         else:
             question = question+chr(65+i)+": "+user+"\n"
         breakpoint
