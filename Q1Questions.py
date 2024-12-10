1afd247e061a5a2b25fe44544f81313b18e8fe74
Enrico Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-12-10 10:39:16 -0500

    Added randomized question support

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,41 +1,37 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
 questionstxt = open("questions.txt", 'w')
+
+def exitcheck(txt):
+    if txt == "EXIT":
+        questionstxt.close()
+        exit()
+
+user = input('Hello! Welcome to the quiz maker.\nTo start, type "RANDOM" if you want the questions to be randomized, or type "NORMAL" for the questions to be in order.\nEnter "EXIT" to quit and save.\n')
+exitcheck(user)
+questionstxt.write(str.capitalize(user)+'\n')
+
 q=1
 while True:
     print("Typing Question",q)
     user = input('Enter your question, enter "EXIT" to quit and save.\n')
-    if user == "EXIT":
-        questionstxt.close()
-        break
+    exitcheck(user)
     question = str(q)+". "+user+"\n"
-    print('Enter 4 answers, and choose the correct answer (A,B,C or D), or enter "EXIT" to quit and save. (E = Correct Answer Letter)')
-    for i in range(5):
-        user = input(str(i+1)+": ")
-        if user == "EXIT":
-            questionstxt.close()
-            exit()
-        if i==4:
+    print('Enter 4 answers, and choose the correct answer (1,2,3 or 4), or enter "EXIT" to quit and save. (5 = Correct Answer Number)')
+    i=1
+    while True:
+        exitcheck(user)
+        if i>4:
+            user = input("5: ")
             try:
                 if int(user)>0 and int(user)<5:
-                    question = question+chr(65+i)+": "+user+"\n"
-                
-        #if i==4:
-        #    if str.capitalize(user)!="A" and str.capitalize(user)!="B" and str.capitalize(user)!="C" and str.capitalize(user)!="D":
-        #        while True:
-        #            print('Please type A,B,C or D.')
-        #            user = input('E: ')
-        #            if user == "EXIT":
-        #                questionstxt.close()
-        #                exit()
-        #            print(user)
-        #            if str.capitalize(user)=="A" or str.capitalize(user)=="B" or str.capitalize(user)=="C" or str.capitalize(user)=="D":
-        #                question = question+chr(65+i)+": "+str.capitalize(user)+"\n"
-        #                break
-        #    else:
-        #        question = question+chr(65+i)+": "+str.capitalize(user)+"\n"
+                    question = question+"5: "+user+"\n"
+                break
+            except:
+                print('Please enter 1,2,3 or 4.')
         else:
-            question = question+chr(65+i)+": "+user+"\n"
-        breakpoint
+            user = input(str(i)+": ")
+            question = question+str(i)+": "+user+"\n"
+            i+=1
     questionstxt.write(question)
     q+=1
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,27 +1,59 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+import random;from datetime import datetime
 questionstxt = open("questions.txt", 'r')
-print('Welcome to the quiz!\nYou can exit the quiz by entering "EXIT"\n')
-streak=0
-while True:
+q=[];a=[]
+txt=questionstxt.readline()
+qtype=txt.strip('\n')
+txt=questionstxt.readline()
+while txt !='':
+    q+=[txt.strip('\n')]
     txt=questionstxt.readline()
-    if txt != '':
-        print('Question',txt.strip('\n'))
+    for _ in range(5):
+        a+=[txt.strip('\n')]
         txt=questionstxt.readline()
+print('Welcome to the quiz!\nYou can exit the quiz by entering "EXIT"\n')
+streak=0;correct=0
+
+def exitcheck(txt):
+    if txt == "EXIT":
+        questionstxt.close()
+        exit()
+question=1
+while len(q)>0:
+    if str.upper(qtype)=='RANDOM':
+        timelol=datetime.now().year*(10**17)+datetime.now().month*(10**15)+datetime.now().day*(10**13)+datetime.now().hour*(10**11)+datetime.now().minute*(10**9)+datetime.now().second*(10**7)+datetime.now().microsecond
+        random.seed = timelol;rand=random.randint(0,len(q)-1)
+        print('Question',str(question)+q[0+rand][1:len(q[0+rand])])
         for i in range(4):
-            print(txt.strip('\n'))
-            txt=questionstxt.readline()
+            print(chr(65+i)+a[i+rand*5][1:len(a[i+rand*5])])
         user=input('Your answer (letter): ')
-        if user == "EXIT":
-            questionstxt.close()
-            exit()
-        elif str.capitalize(user) == txt[3:4]:
-            streak+=1
-            print('\nCongratulations! You got it right. Your answer streak is now',str(streak)+'!\n')
+        exitcheck(user)
+        if str.upper(user) == chr(64+int(a[4+rand*5][3:4])):
+            streak+=1;correct+=1
+            print('\nCongratulations! You got it right. Your answer streak is now',str(streak)+'!\nYour total amount of correct answers is '+str(correct)+'.\n')
         else:
             streak=0
-            print("\nSorry! That's wrong. Your answer streak is back to 0!\nThe correct answer was",txt[3:4]+'!\n')
+            print("\nSorry! That's wrong. Your answer streak is back to 0!\nThe correct answer was",chr(64+int(a[4][3:4]))+'!\nYour total amount of correct answers is '+str(correct)+'.\n')
+        question+=1
+        q.pop(0+rand)
+        for _ in range(5):
+            a.pop(0+rand*4)
     else:
-        print("That's the end of the quiz! Your final answer streak is",str(streak)+'.')
-        questionstxt.close()
-        break
+        print('Question',q[0])
+        for i in range(4):
+            print(chr(65+i)+a[i][1:len(a[i])])
+        print(a)
+        user=input('Your answer (letter): ')
+        exitcheck(user)
+        if str.upper(user) == chr(64+int(a[4][3:4])):
+            streak+=1;correct+=1
+            print('\nCongratulations! You got it right. Your answer streak is now',str(streak)+'!\nYour total amount of correct answers is '+str(correct)+'.\n')
+        else:
+            streak=0
+            print("\nSorry! That's wrong. Your answer streak is back to 0!\nThe correct answer was",chr(64+int(a[4][3:4]))+'!\nYour total amount of correct answers is '+str(correct)+'.\n')
+        q.pop(0)
+        for _ in range(5):
+            a.pop(0)
+print("That's the end of the quiz! Your final answer streak is",str(streak)+'.\nYour final total amount of correct answers is '+str(correct)+'.\n')
+questionstxt.close()
